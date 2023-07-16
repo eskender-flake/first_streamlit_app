96b6e7db4f8630a5924cef7e80de425d5a8e3685
eskender-flake <139680111+eskender-flake@users.noreply.github.com>
2023-07-17 08:43:31 +1000

    Update streamlit_app.py
    fixed appostrophe issue on title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Health Diner')
+streamlit.title('My Parents/' New Health Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
