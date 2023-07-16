7bf43f8d34ed6accba82335bd006032e3538cc3f
eskender-flake <139680111+eskender-flake@users.noreply.github.com>
2023-07-17 08:53:26 +1000

    Update streamlit_app.py
    added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Health Diner')
+streamlit.title('My Mom\'s New Health Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 - Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 - Kale, Spinach & Rocket Smoothie')
