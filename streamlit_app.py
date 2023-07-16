147e59cd79a39cce022cbe8fdd9597ea2ff36adf
eskender-flake <139680111+eskender-flake@users.noreply.github.com>
2023-07-17 08:31:31 +1000

    Update streamlit_app.py
    Added Menu title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,3 @@ streamlit.title('My Parents New Health Diner')
 
 steamlit.body('Breakfast Menu')
 
-steamlit.body('Omega 3 & Blueberry Oatmeal
-              Kale Spinach & Rocket Smoothie
-              Hasd-Boiled Free-Range Egg')
