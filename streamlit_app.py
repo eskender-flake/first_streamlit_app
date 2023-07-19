79adde0987f099cd096e2ea72c3e9d7f7a0abbc4
eskender-flake <139680111+eskender-flake@users.noreply.github.com>
2023-07-19 16:17:03 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 st.header("Fruityvice Fruit Advice!")
-fruit_choice = st.text_input('What fruit would you line information about?','Kiwi')
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered',fruit_choice)
 
 import requests
